db5e2ff3ba0c1b900c06cec163933576ae328aef
Dawitdkt <dawitkinfu5@gmail.com>
2022-04-07 08:32:55 -0700

    advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints "Programming is like building a multilingual puzzle
+ * Return: 0 on succes
+ */
+int main(void)
+{
+	char str[60] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	for (int i = 0, i < 58, i++)
+	{
+		putchar(str[i]); }
+	return (1); }
