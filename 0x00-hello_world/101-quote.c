e635e95d96d0fc5ff46ff649fa13260be50dc7a6
Dawitdkt <dawitkinfu5@gmail.com>
2022-04-07 08:35:04 -0700

    advanced tasks 1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@ int main(void)
 {
 	char str[60] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-	for (int i = 0, i < 58, i++)
+	for (int i = 0; i < 58; i++)
 	{
 		putchar(str[i]); }
 	return (1); }
