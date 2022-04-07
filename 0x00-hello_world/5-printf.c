faca2612c7c5283c0bef2c7de3768f6f1451f085
Dawitdkt <dawitkinfu5@gmail.com>
2022-04-07 07:48:57 -0700

    task 4,5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints "Programming is like building a multilingual puzzle
+ * Return: 0 on succes
+ */
+int main(void)
+{
+
+	printf("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints "Programming is like building a multilingual puzzle
+ * Return: 0 on succes
+ */
+int main(void)
+{
+
+	printf("size of char: %d", sizeof(char));
+	printf("size of int: %d", sizeof(int));
+	printf("size of long int: %d", sizeof(long int));
+	printf("size of long long int: %d", sizeof(long long int));
+	printf("size of float: %d", sizeof(float));
+	return (0); }
