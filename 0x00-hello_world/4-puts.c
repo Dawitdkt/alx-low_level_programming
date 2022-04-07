097cc972ef91f5d0df3be6ebb3531d50ec2f677e
Dawitdkt <dawitkinfu5@gmail.com>
2022-04-07 07:26:30 -0700

    fix on task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - prints "Programming is like building a multilingual puzzle
+ * Return: 0 on succes
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle \n");
+
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0); }
