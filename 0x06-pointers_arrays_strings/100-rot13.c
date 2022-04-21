cefd4a4954ebb372349aeeaa1df47bd709ae6a01
Dawitdkt <dawitkinfu5@gmail.com>
2022-04-21 13:33:44 -0700

    advanced

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,32 +1,31 @@
 #include "main.h"
-
 /**
- * rot13 - Entry point
- * ONE if, TWO loops only...
- * @n: input
- * Return: decrypted string
+ * rot13 - encodes a string using rot13
+ * @str: the string to encode
+ *
+ * Return: encode string
  */
-char *rot13(char *n)
+char *rot13(char *str)
 {
-	int x, rot_c = 13, i = 0;
-	char toswap[] = {'A', 'N', 'a', 'n', 'B', 'O', 'b', 'o', 'C', 'P',
-		'c', 'p', 'D', 'Q', 'd', 'q', 'E', 'R', 'e', 'r', 'F', 'S', 'f',
-		's', 'G', 'T', 'g', 't', 'H', 'U', 'h', 'u', 'I', 'V', 'i', 'v',
-		'J', 'W', 'j', 'w', 'K', 'X', 'k', 'x', 'L', 'Y', 'l', 'y', 'M',
-		'Z', 'm', 'z'};
+	int i, j;
 
-	while (n[i] != '\0')
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	i = 0;
+	while (str[i] != '\0')
 	{
-		for (x = 0; x <= 51; x++)
+		j = 0;
+		while (alpha[j] != '\0')
 		{
-			if (n[i] == toswap[x])
+			if (str[i] == alpha[j])
 			{
-				n[i] = n[i] + rot_c;
-				x = 51;
+				str[i] = rot[j];
+				break;
 			}
-			rot_c = rot_c * -1;
+			j++;
 		}
 		i++;
 	}
-	return (n);
+	return (str);
 }
