3748a4cee0bb289839d7368bfb9eaba43ca96e23
Dawitdkt <dawitkinfu5@gmail.com>
2022-05-20 12:20:03 -0700

    minor fix

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,37 +1,52 @@
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
+
 /**
- * add_node_end - Add a new node at the end of a list
- * @head: Address of the first node of a list
- * @str: Address of the string to insert into the new node
- * Return: Address of the new node
- **/
+ * add_node_end - function that adds a new node at the end of a list_t list
+ * @head: Const double pointer of structure list_t for beginning
+ * @str: Const char pointer for data to be added
+ * Return: List with new node for list_t list, NULL if failed
+ */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *temp, *temp2;
-unsigned int length = 0;
-if (str == NULL)
-return (NULL);
-temp = malloc(sizeof(list_t));
-if (temp == NULL)
-return (NULL);
-temp->str = strdup(str);
-if (temp->str == NULL)
-{
-free(temp);
-return (NULL);
-}
-while (str[length])
-length++;
-temp->len = length;
-temp->next = NULL;
-if (*head == NULL)
-{
-*head = temp;
-return (temp);
-}
-temp2 = *head;
-while (temp2->next)
-temp2 = temp2->next;
-temp2->next = temp;
-return (temp);
+
+	unsigned int i;
+	list_t *new;
+	list_t *temp;
+
+	new  = malloc(sizeof(list_t));
+
+	if (str == NULL)
+		return (NULL);
+
+	if (new == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		new->len = i;
+		new->str = strdup(str);
+		new->next = NULL;
+		return (new);
+	}
+
+	temp = *head;
+
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	temp->next = new;
+
+	new->len = i;
+	new->str = strdup(str);
+	new->next = NULL;
+
+	return (new);
+
 }
